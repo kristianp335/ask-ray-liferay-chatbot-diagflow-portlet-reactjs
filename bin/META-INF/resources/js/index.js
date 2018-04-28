581bbb618b60b133993d819c6c6f90d49e78b3b1
Kris <kris.patefield@liferay.com>
2018-04-28 18:44:47 +0100

    No voice support as yet, or actions

--- bin/META-INF/resources/js/index.js
+++ bin/META-INF/resources/js/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 class AiApiConversation extends React.Component {
 	constructor(props) {
 		  super(props);						
-		  this.state = ({apiAiDataObject: []});		
+		  this.state = ({apiAiDataObject: [], value: ""});	  
+		  this.handleChange = this.handleChange.bind(this);
+    	  this.handleSubmit = this.handleSubmit.bind(this);
 		  this.getApiAiData(); 
   }
   
@@ -24,21 +26,57 @@ class AiApiConversation extends React.Component {
 	  );
   }
 
-  callApiAi() {
-	  alert("Hello World");
+  handleChange(event) {
+	  	  this.setState({value: event.target.value});
   }
+
+  handleSubmit(event) {
+	let apiAiRequest = ({ query: this.state.value,
+	lang: "en",    	
+	sessionId: "12345"
+	});
+	let apiAiRequestString = JSON.stringify(apiAiRequest);
+	$.ajax({
+		url: "https://api.api.ai/v1/query?v=20150910",
+		type: "POST",
+		contentType: "application/json",
+		data: apiAiRequestString,
+		beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer 4304414ee84640ef8267ea82c383d6e9');},
+		success: function(data) {
+		console.log(data);
+		Liferay.Service(
+			'/apiai.apiaidata/add-api-ai-data-persistence',
+			{
+				query: data.result.resolvedQuery, 
+				authtoken: "4304414ee84640ef8267ea82c383d6e9", 
+				speech: data.result.fulfillment.messages["0"].speech, 
+				action: data.result.action, 
+				fulfillment: data.result.fulfillment, 
+				result: data.result
+			},
+			function(obj) {
+				this.getApiAiData();  
+				}.bind(this)
+		).bind(this)
+		
+		}.bind(this)
+	 })
+	 event.preventDefault();
+}
 
   render() {
 	  return (
-		<div>
-			{this.state.apiAiDataObject.map(apiAiDataObjects => (
-			<div className={apiAiDataObjects.type == "query" ? "conversation-query" : "conversation-response"} 
-			key={apiAiDataObjects.speech}>{apiAiDataObjects.speech}</div>
-			))}		
- 		
-			<div><input className="field form-control" onBlur={this.callApiAi} name="ai-query" type="text" value=""/>
+		<form onSubmit={this.handleSubmit}>
+			<div>
+				{this.state.apiAiDataObject.map(apiAiDataObjects => (
+				<div className={apiAiDataObjects.type == "query" ? "conversation-query" : "conversation-response"} 
+				key={apiAiDataObjects.speech}>{apiAiDataObjects.speech}</div>
+				))}		
+			
+				<div><input className="field form-control" onChange={this.handleChange} name="ai-query" type="text" value={this.state.value}/>
+				</div>
 			</div>
-		</div>			  
+		</form>		  
 	  );
 	}
 }
