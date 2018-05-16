b959805a9edb25b075f997ad295b7516594612de
Kris <kris.patefield@liferay.com>
2018-05-16 18:01:03 +0100

    Now supports voice commands in a similar fashion to the MVC Portlet version but much faster due to React

--- bin/META-INF/resources/js/index.js
+++ bin/META-INF/resources/js/index.js
@@ -43,21 +43,21 @@ class AiApiConversation extends React.Component {
 		data: apiAiRequestString,
 		beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer 4304414ee84640ef8267ea82c383d6e9');},
 		success: function(data) {
-		console.log(data);
-		Liferay.Service(
-			'/apiai.apiaidata/add-api-ai-data-persistence',
-			{
-				query: data.result.resolvedQuery, 
-				authtoken: "4304414ee84640ef8267ea82c383d6e9", 
-				speech: data.result.fulfillment.messages["0"].speech, 
-				action: data.result.action, 
-				fulfillment: data.result.fulfillment, 
-				result: data.result
-			},
-			function(obj) {
-				this.getApiAiData();  
-				}.bind(this)
-		).bind(this)
+			console.log(data);
+			Liferay.Service(
+				'/apiai.apiaidata/add-api-ai-data-persistence',
+				{
+					query: data.result.resolvedQuery, 
+					authtoken: "4304414ee84640ef8267ea82c383d6e9", 
+					speech: data.result.fulfillment.messages["0"].speech, 
+					action: data.result.action, 
+					fulfillment: data.result.fulfillment, 
+					result: data.result
+				},
+				function(obj) {
+					this.getApiAiData();  
+					}.bind(this)
+			)
 		
 		}.bind(this)
 	 })
@@ -70,7 +70,7 @@ class AiApiConversation extends React.Component {
 			<div>
 				{this.state.apiAiDataObject.map(apiAiDataObjects => (
 				<div className={apiAiDataObjects.type == "query" ? "conversation-query" : "conversation-response"} 
-				key={apiAiDataObjects.speech}>{apiAiDataObjects.speech}</div>
+				key={apiAiDataObjects.apiAiDataId}><b>{apiAiDataObjects.type == "query" ? "You said..." : "Ray said..."}</b><br/>{apiAiDataObjects.speech}</div>
 				))}		
 			
 				<div><input className="field form-control" onChange={this.handleChange} name="ai-query" type="text" value={this.state.value}/>
--- src/main/resources/META-INF/resources/js/index.js
+++ src/main/resources/META-INF/resources/js/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 class AiApiConversation extends React.Component {
 	constructor(props) {
 		  super(props);						
-		  this.state = ({apiAiDataObject: [], value: ""});	  
+		  this.state = ({apiAiDataObject: [], value: "", isItVoice: false});	  
 		  this.handleChange = this.handleChange.bind(this);
-    	  this.handleSubmit = this.handleSubmit.bind(this);
+		  this.handleSubmit = this.handleSubmit.bind(this);
+		  this.recordVoice = this.recordVoice.bind(this);
 		  this.getApiAiData(); 
-  }
+ }
   
   getApiAiData() {
 	  Liferay.Service(
@@ -29,6 +30,25 @@ class AiApiConversation extends React.Component {
   handleChange(event) {
 	  	  this.setState({value: event.target.value});
   }
+
+  recordVoice(event) {
+		this.setState({isItVoice : true});
+		let SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+		let SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+		let recognitionReact = new SpeechRecognition();
+		recognitionReact.lang = 'en-UK';
+		let synth = window.speechSynthesis;
+		let msg = new SpeechSynthesisUtterance();	
+		recognitionReact.start();
+		recognitionReact.onresult = function(event) {
+			var speechResultReact = this.state.value;
+			speechResultReact = event.results["0"]["0"].transcript;
+			this.setState({value : speechResultReact});
+			this.handleSubmit(event);
+	  	}.bind(this)
+  }
+
+  
 
   handleSubmit(event) {
 	let apiAiRequest = ({ query: this.state.value,
@@ -55,7 +75,16 @@ class AiApiConversation extends React.Component {
 					result: data.result
 				},
 				function(obj) {
-					this.getApiAiData();  
+					this.getApiAiData();
+					if(this.state.isItVoice == true)
+						{
+							let synth = window.speechSynthesis;
+							let msg = new SpeechSynthesisUtterance();
+							msg.text = data.result.fulfillment.messages["0"].speech;
+							synth.speak(msg);
+						}
+					this.setState({isItVoice : false});
+					this.setState({value: ""});
 					}.bind(this)
 			)
 		
@@ -70,10 +99,16 @@ class AiApiConversation extends React.Component {
 			<div>
 				{this.state.apiAiDataObject.map(apiAiDataObjects => (
 				<div className={apiAiDataObjects.type == "query" ? "conversation-query" : "conversation-response"} 
-				key={apiAiDataObjects.apiAiDataId}>{apiAiDataObjects.speech}</div>
+				key={apiAiDataObjects.apiAiDataId}><b>{apiAiDataObjects.type == "query" ? "You said..." : "Ray said..."}</b><br/>{apiAiDataObjects.speech}</div>
 				))}		
 			
 				<div><input className="field form-control" onChange={this.handleChange} name="ai-query" type="text" value={this.state.value}/>
+				</div>
+				<div>
+					<p>
+						<br/>					
+						<button className="btn icon-microphone btn-default" onClick={this.recordVoice}  name="micbutton" type="text"/>						
+					</p>
 				</div>
 			</div>
 		</form>		  
